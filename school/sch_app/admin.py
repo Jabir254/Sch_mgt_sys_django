f18b0d501581d86c3ee9d84fdc5e876df170ce77
jabir254 <jabirkullow@gmail.com>
2023-06-14 15:35:06 +0300

    added models into admin page

--- school/sch_app/admin.py
+++ school/sch_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . models import std_mgt, Staff, LibraryRecord, Course_mgt, Attendance, Grade_mgt, Transcript, Timetable, Book, Finance, FeeCategory, Payment
 # Register your models here.
+
+ 
